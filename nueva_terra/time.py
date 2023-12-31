258f03987d4390f8d9810e97ae4eb070562c24bd
William Issac Whitaker <70265801+WillBytes@users.noreply.github.com>
2023-12-31 01:27:05 +0000

    Update time.py
    Updated with the ability to read time.txt outputted from nuevaterratimeretrieval.

--- nueva_terra/time.py
+++ nueva_terra/time.py
@@ -1,26 +1,36 @@
-from datetime import UTC, datetime, timedelta
+from datetime import datetime, timedelta
 
-from nextcord.utils import utcnow
+def read_time_from_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            time_data = file.readline()
+            return int(time_data.strip().split('=')[1])
+    except IOError as e:
+        print(f"Error reading file: {e}")
+        return None
 
-ANP = datetime(year=2023, month=1, day=1, tzinfo=UTC)
-ZERO = datetime(year=1, month=1, day=1, tzinfo=UTC)
+def minecraft_time_to_real_time(ticks):
+    # There are 24000 ticks in a Minecraft day which is equivalent to 24 hours
+    seconds_per_tick = 24 * 60 * 60 / 24000
+    real_time_seconds = ticks * seconds_per_tick
 
+    # Minecraft day 1, 6:00 AM corresponds to the 0th tick
+    start_time = datetime(year=1, month=1, day=1, hour=6)
+    return start_time + timedelta(seconds=real_time_seconds)
 
-def time_since_anp() -> timedelta:
-    return utcnow() - ANP
+def current():
+    time_ticks = read_time_from_file('path/to/time.txt')  # Update this with the correct path
+    if time_ticks is not None:
+        return minecraft_time_to_real_time(time_ticks)
+    else:
+        return None
 
-
-def current() -> datetime:
-    since_anp = time_since_anp().total_seconds()
-
-    return ZERO + (timedelta(seconds=since_anp) * 8)
-
-
-def current_format() -> str:
-    datetime = current()
-    date = datetime.strftime("%d %b %Y ANP")
-
-    time = datetime.strftime("%H:%M")
-    time = time.rjust(len(date))
-
-    return f"{date}\n\n{time}"
+def current_format():
+    current_time = current()
+    if current_time is not None:
+        date = current_time.strftime("%d %b %Y ANP")
+        time = current_time.strftime("%H:%M")
+        time = time.rjust(len(date))
+        return f"{date}\n\n{time}"
+    else:
+        return "Time data not available"
