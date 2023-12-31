248df4840937837ebcd1a6e29a3bd359ab6360a8
William Issac Whitaker <70265801+WillBytes@users.noreply.github.com>
2023-12-31 22:36:58 +0000

    Update time.py
    Rewritten to create a custom calendar for Nueva Terra.

--- nueva_terra/time.py
+++ nueva_terra/time.py
@@ -1,36 +1,57 @@
 from datetime import datetime, timedelta
 
-def read_time_from_file(/Desktop/Nueva Terra/time.txt):
+# Custom Calendar Configuration
+MONTH_NAMES = ["HER", "POS", "ARE", "APH", "ART",
+               "HRM", "APO", "DIO", "DEM", "HES",
+               "HEP", "ATH", "ZEU", "PER"]
+
+def read_time_from_file(file_path):
     try:
         with open(file_path, 'r') as file:
             time_data = file.readline()
-            return int(time_data.strip().split('=')[1])
+            return int(time_data.strip())
     except IOError as e:
         print(f"Error reading file: {e}")
         return None
+    except ValueError as e:
+        print(f"Error converting file data to integer: {e}")
+        return None
 
-def minecraft_time_to_real_time(ticks):
-    # There are 24000 ticks in a Minecraft day which is equivalent to 24 hours
+def minecraft_time_to_custom_calendar_time(ticks):
+    # Adjust the ratio for 420-day year
     seconds_per_tick = 24 * 60 * 60 / 24000
-    real_time_seconds = ticks * seconds_per_tick
+    total_seconds = ticks * seconds_per_tick
+    total_seconds += 6 * 60 * 60  # Add 6 hours
+    total_days = int(total_seconds // (24 * 60 * 60))
 
-    # Minecraft day 1, 6:00 AM corresponds to the 0th tick
-    start_time = datetime(year=1, month=1, day=1, hour=6)
-    return start_time + timedelta(seconds=real_time_seconds)
+    # Calculate the custom calendar date
+    year = total_days // 420 + 20  # Starting from year 20
+    day_of_year = total_days % 420
+    month_index = int(day_of_year // 30)
+    month_day = day_of_year % 30 + 1  # Day of the month
+    week = (month_day - 1) // 6 + 1
+    day_of_week = (month_day - 1) % 6 + 1
+
+    # Calculate time of day
+    hours = int(total_seconds % (24 * 60 * 60) // 3600)
+    minutes = int(total_seconds % 3600 // 60)
+
+    # Format the custom date-time string
+    date_str = f"{month_day:02d} {MONTH_NAMES[month_index]} {year}"
+    time_str = f"{hours:02d}:{minutes:02d}"
+    time_str = time_str.rjust(len(date_str))
+    return f"{date_str}\n\n{time_str}"
 
 def current():
-    time_ticks = read_time_from_file('/Desktop/Nueva Terra/time.txt')  # Update this with the correct path
+    time_ticks = read_time_from_file('/app/data/time.txt')
     if time_ticks is not None:
-        return minecraft_time_to_real_time(time_ticks)
+        return minecraft_time_to_custom_calendar_time(time_ticks)
     else:
         return None
 
 def current_format():
     current_time = current()
     if current_time is not None:
-        date = current_time.strftime("%d %b %Y ANP")
-        time = current_time.strftime("%H:%M")
-        time = time.rjust(len(date))
-        return f"{date}\n\n{time}"
+        return current_time
     else:
         return "Time data not available"
